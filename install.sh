4e100b85e11f3aef76045078865c0cb289c5b99f
Bendik Egenes Dyrli <skandix@users.noreply.github.com>
2014-12-20 11:12:06 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,31 +1,47 @@
 #!/bin/bash
+# Rmbr git
 echo "Doing updates and such to make things go faster.. to save time"
 sleep 2
 clear
 echo "Copying dotfiles"
-cp /.bashrc /home/*/
-cp /.bashrc /root
-cp /.conkyrc /home/*/
+cp ../confis/.bashrc $home
+cp ../configs/.bashrc /root/
+cp ../configs/.conkyrc $home
+sleep 3
 echo "Dotfiles copied to all users and root"
 sleep 3
 clear
-echo "installing updates and software"
+echo "Add your user to visudo $username ALL=(ALL:ALL) ALL"
 sleep 2
 clear
-# Doing a update on all the sources.list to get the newest updates, then install updates and then dist-upgrade if any new updates
-apt-get update -y && apt-get upgrade -y && apt-get dist upgrade -y
-# Installing Awesome WM
-apt-get install awesome
-# installing sublime text 2
-add-apt-repository ppa:webupd8team/sublime-text-2
+visudo
+clear
+Echo "Adding lines to sources.list and installing software, now you can safely go whatever you want to do.."
+sleep 2
+# adding experimental awesome to sources.list with echo
+echo “#EXPERIMENTAL
+deb http://ftp.us.debian.org/debian/ experimental main contrib non-free
+#SID
+deb http://ftp.us.debian.org/debian/ sid main contrib non-free
+deb http://http.debian.net/debian/ wheezy main non-free contrib
+deb-src http://http.debian.net/debian/ wheezy main non-free contrib” >> /etc/apt/sources.list
+# Installing Awesome WM EXPERIMENTAL
 apt-get update
-apt-get install sublime-text
+apt-get install xorg awesome alsa-utils vim screenfetch
+apt-get -t experimental install awesome 
+# Stop Pc Beep
+modprobe -r pcspkr
+echo "# Do not load 'pcspkr' module on boot
+blacklist pcspkr" >> /etc/modprobe.d/nobeep.conf
+# installing wifi wicd
+aptitude update
+aptitude install wicd
 # Installing Chromium Browser
 apt-get install chromium-browser
 # Installing some tools i use / need
-apt-get install screen vim bmon irssi tmux git nload conky macchanger make python-dev iptraf steam 
+apt-get install screen bmon irssi tmux git nload conky macchanger make python-dev iptraf  
 # adding repo, keys to install Spotify
-apt-add-repository -y "deb http://repository.spotify.com stable non-free"
+echo "deb http://repository.spotify.com stable non-free" >> /etc/apt/sources.list
 apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 94558F59
 apt-get update -qq
 apt-get install spotify-client	
@@ -37,26 +53,19 @@ update-pepperflashplugin-nonfree --status
 apt-get update -y
 # installing VLC, just because i'm not sure if MPC:HC is on linux yet :P
 apt-get install vlc browser-plugin-vlc
-# adding repo to Transmission, torrent client for linux
-add-apt-repository ppa:transmissionbt/ppa
-apt-get update 
-apt-get install transmission
-# Google drive for linux
-add-apt-repository ppa:thefanclub/grive-tools
-apt-get update
-apt-get install gdrive-tools
-# adding screenfetch repo, and install of it
-apt-add-repository ppa:djcj/screenfetch
-apt-get update
-apt-get install screenfetch
+# installing rtorrent, torrent client for linux
+apt-get install rtorrent
 #installing ipcalc
 apt-get install ipcalc
 # installing java
-apt-add-repository ppa:webupd8team/java
-apt-get update
 apt-get install oracle-java8-installer
 #installing gimp 
 apt-get install gimp
+#copycats theme pack
+cd $home
+git clone --recursive https://github.com/copycat-killer/awesome-copycats.git
+cd awesome-copycats && cp ~/awesome-copycats/ ~/.config/awesome
+clear
 echo "Done installing updates and software"
 sleep 2
 echo "Have a good day ;)"
