d2918e7b5c6e1eec5e43402d7c6b8251876c5c68
Bendik Egenes Dyrli <skandix@users.noreply.github.com>
2015-12-13 21:59:09 +0100

    Update install_.sh
    update

--- install_.sh
+++ install_.sh
@@ -1,14 +1,14 @@
 apt-get install screen bmon irssi tmux git nload conky macchanger make python-dev iptraf -y 
-echo "deb http://repository.spotify.com stable non-free" >> /etc/apt/sources.list
-apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 94558F59
-apt-get update -qq
-apt-get install spotify-client	-y
+apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys BBEBDCB318AD50EC6865090613B00F1FD2C1988
+echo deb http://repository.spotify.com stable non-free | sudo tee /etc/apt/sources.list.d/spotify.list
+apt-get update
+apt-get install spotify-client
 apt-get install pepperflashplugin-nonfree -y
 update-pepperflashplugin-nonfree --install
 update-pepperflashplugin-nonfree --status
 apt-get update -y
-apt-get install vlc browser-plugin-vlc -y
 apt-get install rtorrent -y
 apt-get install ipcalc -y
-apt-get install oracle-java8-installer -y
-apt-get install gimp -y
+apt-get install alsa-utils -y
+apt-get install rxvt-unicode-256color
+apt-get install sudo
