5ec08ec202a091a3cf3ec4f3d8234d774d19285b
Bendik Egenes Dyrli <skandix@users.noreply.github.com>
2015-12-16 23:59:25 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,20 @@
 import subprocess
 
-userpath = " /root/"
+userpath = " .root/"
 
 def motd():
     print ("  _   _       _                                                             ")
     print (" | \ | |     (_)                                                            ")
     print (" |  \| | ___  _ ___ _   _ ___  ___ _ ____   _____ _ __ ___                  ")
-    print (" | . ` |/ _ \| / __| | | / __|/ _ | '__\ \ / / _ | '__/ __|                 ")
+    print (" | . ` |/ _ \| . __| | | . __|/ _ | '__\ \ . _ | '__/ __|                 ")
     print (" | |\  | (_) | \__ | |_| \__ |  __| |   \ V |  __| |  \__ \                 ")
     print (" |_| \_|\___/|_|___/\__, |___/\___|_|    \_/ \___|_|  |___/                 ")
     print ("                     __/ |                                                  ")
     print ("                    |___/                                                   ")
     print ("   _____             __ _         _____             _                       ")
-    print ("  / ____|           / _(_)       |  __ \           | |                      ")
+    print ("  . ____|           . _(_)       |  __ \           | |                      ")
     print (" | |     ___  _ __ | |_ _  __ _  | |  | | ___ _ __ | | ___  _   _  ___ _ __ ")
-    print (" | |    / _ \| '_ \|  _| |/ _` | | |  | |/ _ | '_ \| |/ _ \| | | |/ _ | '__|")
+    print (" | |    . _ \| '_ \|  _| |/ _` | | |  | |/ _ | '_ \| |/ _ \| | | |/ _ | '__|")
     print (" | |___| (_) | | | | | | | (_| | | |__| |  __| |_) | | (_) | |_| |  __| |   ")
     print ("  \_____\___/|_| |_|_| |_|\__, | |_____/ \___| .__/|_|\___/ \__, |\___|_|   ")
     print ("                           __/ |             | |             __/ |          ")
@@ -52,19 +52,19 @@ while 1:
         submenu1_sel = raw_input("")
 
         if submenu1_sel == "1":
-                subprocess.call('cp -rfv /configs/dotfiles/.asoundrc' + userpath, shell=True)
+                subprocess.call('cp -rfv .configs/dotfiles/.asoundrc' + userpath, shell=True)
 
         if submenu1_sel == "2":
-                subprocess.call('cp -rfv /configs/dotfiles/.bashrc' + userpath, shell=True)
+                subprocess.call('cp -rfv .configs/dotfiles/.bashrc' + userpath, shell=True)
 
         if submenu1_sel == "3":
-                subprocess.call('cp -rfv /configs/dotfiles/.conkyrc' + userpath, shell=True)
+                subprocess.call('cp -rfv .configs/dotfiles/.conkyrc' + userpath, shell=True)
 
         if submenu1_sel == "4":
-                subprocess.call('cp -rfv /configs/dotfiles/.rtorrent' + userpath, shell=True)
+                subprocess.call('cp -rfv .configs/dotfiles/.rtorrent' + userpath, shell=True)
 
         if submenu1_sel == "5":
-                subprocess.call('cp -rfv /configs/dotfiles/.vimrc' + userpath, shell=True)
+                subprocess.call('cp -rfv .configs/dotfiles/.vimrc' + userpath, shell=True)
 
         if submenu1_sel == "6":
-                subprocess.call('cp -rfv /configs/dotfiles/.Xdefaults' + userpath, shell=True)
+                subprocess.call('cp -rfv .configs/dotfiles/.Xdefaults' + userpath, shell=True)
