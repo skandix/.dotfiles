ec30abceb65265a21bea517dc2d6bd29f1b1f26e
skandix <skandix@users.noreply.github.com>
2017-03-07 19:08:21 +0100

    Update install.sh
    to damn tired of this shit

--- install.sh
+++ install.sh
@@ -1,14 +1,11 @@
 #/bin/sh
 # Refreshing Repo's
+user="skandix"
 apt-get update
-apt-get install zsh -y
-
-#install oh-my-zsh
-sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-#adding the .theme file i customized later :)
+apt-get install zsh curl -y
 
 # Installing the Packages i need
-apt-get install cmus vlc awesome xbacklight xorg screen pulseaudio pavucontrol bmon vim irssi curl tmux nload conky macchanger make python-dev python-pip chromium wicd-curses alsa-utils ipcalc rxvt-unicode-256color sudo iptraf iwconfig  -y 
+apt-get install cmus mpv awesome xbacklight xorg screen pulseaudio pavucontrol bmon vim tmux nload macchanger make python-dev python-pip chromium wicd-curses alsa-utils ipcalc rxvt-unicode-256color sudo iptraf iwconfig  -y 
 
 # Spotify Client for Linux
 apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys BBEBDCB318AD50EC6865090613B00F1FD2C1988
@@ -16,18 +13,9 @@ echo deb http://repository.spotify.com stable non-free | sudo tee /etc/apt/sourc
 apt-get update
 apt-get install spotify-client
 
-# Pepperflash is Flash for linux
-apt-get install pepperflashplugin-nonfree -y
-update-pepperflashplugin-nonfree --install
-update-pepperflashplugin-nonfree --status
-
 #Sublime Text 3 x64
 wget http://c758482.r82.cf2.rackcdn.com/sublime-text_build-3083_amd64.deb
 dpkg -i sublime-text_build-3083_amd64.deb 
-
-# Sublime Text 3 i386
-# wget http://c758482.r82.cf2.rackcdn.com/sublime-text_build-3083_i386.deb
-# sudo dpkg -i sublime-text_build-3083_i386.deb
 
 # Stop BEEPING sound from internal speaker
 modprobe -r pcspkr
@@ -48,4 +36,9 @@ apt-get -t experimental install awesome -y
 # making awesome folder visible in .config at your homepath
 echo "mkdir -p ~/.config/awesome/"
 echo "cp /etc/xdg/awesome/rc.lua ~/.config/awesome/"
-echo "chown 'whoami' ~/.config/awesome/rc.lua"
+echo "chown whoami ~/.config/awesome/rc.lua"
+#install oh-my-zsh
+sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+cp dotfiles/lambda.zsh-theme /home/$user/.oh-my-zsh/themes/
+cp dotfiles/.zshrc /home/$user/
