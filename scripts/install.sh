c1c001a1f3ff67639bb5d54fe65e44bb48ec2a47
Bendik λ <skandix@datapor.no>
2018-01-05 12:42:25 +0100

    Update install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -10,96 +10,120 @@ echo $'|___/ \__,_|\__\__,_| .__/ \___/|_|(_) |_| |_|\___/ '
 echo $'                    | |                             '
 echo $'                    |_|                             '
 echo ""
-echo -n $'\E[39m'
+echo $(NC)
 
-dir=~/.dotfiles/files
-olddir=~/.dotfiles_old
+misc=~/.dotfiles/misc
+dotdir=~/.dotfiles/dotfiles
+
+selUser=~/$1
+selHome=~/home/$selUser
+
+NC='\033[0m'
+RED='\033[0;31m'
+ORANGE='\033[0;33m'
+YELLOW='\033[1;33m'
+GREEN='\033[0;32m'
+BLUE='\033[0;34m'
+PURPLE='\033[0;35m'
 
 # Install all dotfiles
 dotfiles=".moc .vim .bashrc .gitconfig .vimrc .Xdefaults .zshrc .Xresources"
 for dotfile in $dotfiles; do
-    printf "Installing %s...\n" $dotfile    
-    ln -fs /home/$1/.dotfiles/files/$dotfile ~/$dotfile 2>/dev/null
+    printf "[+] symlinking %s...\n" $dotfile
+    ln -fs $selHome/$dotdir/$dotfile ~/$dotfile 2>/dev/null
 done
 
 
 # Install awesome & Xorg?
-read -p "Install Awesome & Xorg? Y/n " option
-echo
-case "$option" in
-    vy|Y ) echo "Yes";
-        echo "Installing Xorg";
-        sudo apt-get install xorg;
-        echo "Installing Awesome";
-        sudo apt-get install awesome;
-        echo "Copying configs"
-        sudo mkdir -p /home/$1/.config/awesome/;
-        sudo cp /etc/xdg/awesome/rc.lua /home/$1/.config/awesome/;
-        sudo chown $1 /home/$1/.config/awesome/rc.lua;;        
-    n|n ) echo "No";;
-    * ) echo "Invalid option";;
-esac
-
-
-
-# Add Public key
-read -p "Add public key ? Y/n " option
-echo
-case "$option" in
-    y|Y) echo "Yes";
-        mkdir -p /home/$1/.ssh/;
-        touch /home/$1/.ssh/authorized_keys;
-        wget -qO - $2 >> /home/$1/.ssh/authorized_keys;;
-    n|N ) echo "No";;
-    * ) echo "Invalid option";;
-esac
-
-# Install Packages?
-read -p "What Packages ? 1: Laptop, 2: Workstation, 3: Server, 4: Minimal Server n/N " option
-echo
-case "$option" in
-    1 ) echo "Laptop"; sudo apt-get install screenfetch vim xbacklight mpv screen pulseaudio pavucontrol tmux python-dev python-pip chromium wicd-curses alsa-utils rxvt-unicode-256color zsh moc -y;;
-    2 ) echo "Workstation"; sudo apt-get install screenfetch vim mpv screen pulseaudio pavucontrol tmux python3-dev python3-pip python-dev python-pip alsa-utils rxvt-unicode-256color zsh moc virtualenv virtualenvwrapper dirmngr -y;;
-    3 ) echo "Server"; sudo apt-get install screenfetch screen tmux python3-dev python3-pip python-dev python-pip virtualenvwrapper virtualenv zsh vim -y;;
-    4 ) echo "Minimal Server"; sudo apt-get install screenfetch screen tmux zsh vim -y;;
-    n|N ) echo "No";;
-    * ) echo "Invalid option";;
-esac
-
-# Install Vim plugins?
-read -p "Install Vim plugins & oh-my-zsh? Y/n " option
+read -p "$(RED)Install Awesome & Xorg? y/n " option
 echo
 case "$option" in
     y|Y ) echo "Yes";
-        echo "Installing Vundle";
-        git clone https://github.com/gmarik/Vundle.vim.git /home/$1/.vim/bundle/Vundle.vim;
+        echo "[+] Installing Xorg";
+        sudo apt-get install xorg;
 
-	echo "Installing oh-my-zsh";
-        git clone git://github.com/robbyrussell/oh-my-zsh.git /home/$1/.oh-my-zsh;
-        vim +PluginInstall +qall;
-        ln -s $dir/Trilambda.zsh-theme /home/$1/.oh-my-zsh/themes/Trilambda.zsh-theme -f;;
+        echo "[+] Installing Awesome";
+        sudo apt-get install awesome;
 
+        echo "[+] Copying configs"
+        sudo mkdir -p $selHome/.config/awesome/;
+        
+        sudo cp /etc/xdg/awesome/rc.lua $selHome/.config/awesome/;
+        sudo chown $selUser $selHome/.config/awesome/rc.lua;;        
     n|n ) echo "No";;
     * ) echo "Invalid option";;
 esac
+echo "\n"
+echo "$(NC)"
+
+# Add Public key
+read -p "$(ORANGE)Add public key ? y/n " option
+echo
+case "$option" in
+    y|Y) echo "Yes";
+        mkdir -p $selHome/.ssh/;
+        touch $selHome/.ssh/authorized_keys;
+        wget -qO - $2 >> $selHome/.ssh/authorized_keys;;
+    n|N ) echo "No";;
+    * ) echo "Invalid option";;
+esac
+echo "\n"
+echo "$(NC)"
+
+# Install Packages?
+read -p "$(YELLOW)What Packages ? 1: Laptop, 2: Workstation, 3: Server, 4: Minimal Server n/N " option
+echo
+case "$option" in
+    1 ) echo "Laptop"; sudo apt-get install screenfetch vim mpv screen pulseaudio pavucontrol tmux python3-dev python3-pip python-dev python-pip alsa-utils rxvt-unicode-256color zsh moc virtualenv virtualenvwrapper dirmngr xbacklight wicd-curses etckeep -y;;
+    2 ) echo "Workstation"; sudo apt-get install screenfetch vim mpv screen pulseaudio pavucontrol tmux python3-dev python3-pip python-dev python-pip alsa-utils rxvt-unicode-256color zsh moc virtualenv virtualenvwrapper dirmngr etckeep -y;;
+    3 ) echo "Server"; sudo apt-get install screenfetch vim screen tmux python3-dev python3-pip python-dev python-pip zsh virtualenv virtualenvwrapper dirmngr etckeep -y;;
+    4 ) echo "Minimal Server"; sudo apt-get install screenfetch screen tmux zsh vim etckeep -y;;1
+    n|N ) echo "No";;
+    * ) echo "Invalid option";;
+esac
+echo "\n"
+echo "$(NC)"
+
+# Install Vim plugins?
+read -p "$(GREEN)Install Vim plugins & oh-my-zsh? y/n " option
+echo
+case "$option" in
+    y|Y ) echo "Yes";
+        echo "[+] Installing Vundle";
+        git clone https://github.com/gmarik/Vundle.vim.git $selHome/.vim/bundle/Vundle.vim;
+
+    	echo "[+] Installing oh-my-zsh";
+        git clone git://github.com/robbyrussell/oh-my-zsh.git $selHome/.oh-my-zsh;
+        vim +PluginInstall +qall;
+        ln -s $misc/Trilambda.zsh-theme $selHome/.oh-my-zsh/themes/Trilambda.zsh-theme -f;;
+
+    n|N ) echo "No";;
+    * ) echo "Invalid option";;
+esac
+echo "\n"
+echo "$(NC)"
 
 # install Telegram
-read -p "Install Telegram?  y/n " option
+read -p "$(BLUE)Install Telegram?  y/n " option
 echo
 case "$option" in
-    y|Y ) echo "Install"; cd /tmp && wget https://telegram.org/dl/desktop/linux && tar xvf linux && sudo mv Telegram /opt/Telegram && sudo chown $1:$1 /opt/Telegram -R && sudo ln -s /opt/Telegram/Telegram /bin/Telegram && sudo ln -s /opt/Telegram/Updater /bin/Updater;;
+    y|Y ) echo "Install"; cd /tmp && wget -O linux https://telegram.org/dl/desktop/linux && tar xvf linux && sudo mv Telegram /opt/Telegram && sudo chown $selUser:$selUser /opt/Telegram -R && sudo ln -s /opt/Telegram/Telegram /bin/Telegram && sudo ln -s /opt/Telegram/Updater /bin/Updater;;
     n|N ) echo "No";;
     * ) echo "Invalid option";;
 esac
+echo "\n"
+echo "$(NC)"
 
 # install firefox
-read -p "Install firefox?  y/n " option
+read -p "$(PURPLE)Install firefox?  y/n " option
 echo
 case "$option" in
-    y|Y ) echo "Install"; cd /tmp && wget -O firefax https://download.mozilla.org/\?product\=firefox-latest-ssl\&os\=linux64\&lang\=en-US && tar xvf firefax && sudo mv firefox /opt/ && sudo chown $1:$1 /opt/firefox -R && sudo ln -s /opt/firefox/firefox /bin/firefox;;
+    y|Y ) echo "Install"; cd /tmp && wget -O firefax https://download.mozilla.org/\?product\=firefox-latest-ssl\&os\=linux64\&lang\=en-US && tar xvf firefax && sudo mv firefox /opt/ && sudo chown $selUser:$selUser /opt/firefox -R && sudo ln -s /opt/firefox/firefox /bin/firefox;;
     n|N ) echo "No";;
     * ) echo "Invalid option";;
 esac
+echo "\n"
+echo "$(NC)"
 
 # Install Spotify?
 read -p "Install/ Update Spotify? 1: Install, 2:Update, n/N " option
@@ -110,12 +134,17 @@ case "$option" in
     n|N ) echo "No";;
     * ) echo "Invalid option";;
 esac
+echo "\n"
+echo "$(NC)"
+
+
 
 # plz stop thefuckin beeping...!!!
 sudo modprobe -r pcspkr
 echo "# Do not load 'pcspkr' module on boot "\n "#blacklist pcspkr" | sudo tee -a /etc/modprobe.d/nobeep.conf
 sudo mv /etc/motd /etc/motd.back
-cd /home/$1/.config/awesome
-sudo cp rc.lua rc.lua.bak
-cp /home/$1/.dotfiles/files/rc.lua /home/$1/.config/awesome -rfv
-sudo chown $1:$1 /home/$1 -R
+
+sudo chown $selUser:$selUser $selHome -R
+cd $selHome/.config/awesome/
+cp rc.lua rc.lua.bak
+cp $selHome/.dotfiles/files/rc.lua $selHome/.config/awesome -rfv
