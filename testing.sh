b55668f51aad3eb35ad81ce950b619329e448403
Bendik λ <skandix@datapor.no>
2018-05-26 01:01:45 +0200

    getting tired of this

--- testing.sh
+++ testing.sh
@@ -1,7 +1,8 @@
 # implement this as well here https://wiki.debian.org/UnattendedUpgrades
 
 # set debian flavor
-read -p "What Debian Flavor do you want ?\n1: Jessie (Oldstable)\n2: Stretch (Stable)\n3:Buster (Testing)\n4:Sid (Unstable)\n5: Roll backup" option
+read -p "What Debian Flavor do you want ?" option
+echo "\n1: Jessie (Oldstable)\n2: Stretch (Stable)\n3:Buster (Testing)\n4:Sid (Unstable)\n5: Roll backup" 
 echo  
 case "$option" in
     1 ) echo "Jessie (Oldstable)"; cd /etc/apt/ && sudo cp sources.list sources.list.bak && echo "# Debian Jessie (Oldstable)\ndeb http://deb.debian.org/debian/ oldstable main contrib non-free\n#Security\ndeb http://deb.debian.org/debian-security oldstable/updates main\n" | sudo tee sources.list ;; 
