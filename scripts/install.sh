0da7c1658d270e0f14e0766057bc64e8d480ca59
Bendik λ <skandix@datapor.no>
2018-05-25 22:58:36 +0200

    fuck fuck fuck fuck fuck fuck fuck fuck
    fuck fuck fuck fuck fuck fuck fuck fuck fuck fuck fuck fuck fuck fuck fuck fuck fuck fuck fuck fuck fuck fuck fuck fuck fuck fuck fuck fuck fuck fuck fuck fuck fuck fuck fuck fuck fuck fuck fuck fuck fuck fuck fuck fuck fuck fuck fuck fuck fuck fuck fuck fuck fuck fuck fuck fuck fuck fuck fuck fuck fuck fuck fuck fuck fuck fuck fuck fuck fuck fuck fuck fuck fuck fuck fuck fuck fuck fuck fuck fuck fuck fuck fuck fuck

--- scripts/install.sh
+++ scripts/install.sh
@@ -30,7 +30,7 @@ permissions()
 
 function dotfiles {
 dir=~/.dotfiles/files
-dotfiles=(".moc .vim .bashrc .gitconfig .vimrc .Xdefaults .zshrc .Xresources")
+dotfiles=".moc .vim .bashrc .gitconfig .vimrc .Xdefaults .zshrc .Xresources"
 for dotfile in $dotfiles; do
     printf "Installing %s...\n" $dotfile    
     ln -svf /home/$1/.dotfiles/files/$dotfile ~/$dotfile 2>/dev/null
