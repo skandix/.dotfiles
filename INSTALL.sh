50e65767f26d83ec2d038ba48bfc593bc59e789c
Bendik λ <skandix@datapor.no>
2018-05-27 14:54:03 +0200

    cosmetic
    cosmetic

--- INSTALL.sh
+++ INSTALL.sh
@@ -44,11 +44,30 @@ echo
 read -p "$(echo -e 'What Debian Flavor do you want ?\n1: Jessie (Oldstable)\n2: Stretch (Stable)\n3: Buster (Testing)\n4: Sid (Unstable)\n5: Roll backup\n\b')" option
 echo  
 case "$option" in
-    1 ) echo "Jessie (Oldstable)"; cd /etc/apt/; sudo cp sources.list sources.list.bak; echo "# Debian Jessie (Oldstable)\ndeb http://deb.debian.org/debian/ oldstable main contrib non-free\n#Security\ndeb http://deb.debian.org/debian-security oldstable/updates main\n" | sudo tee sources.list;; 
-    2 ) echo "Stretch (Stable)"; cd /etc/apt/; sudo cp sources.list sources.list.bak; echo "# Debian Stretch (Stable)\ndeb http://deb.debian.org/debian/ stable main contrib non-free\ndeb-src http://deb.debian.org/debian/ stable main contrib non-free\n# Security\ndeb http://deb.debian.org/debian-security stable/updates main\ndeb-src http://deb.debian.org/debian-security stable/updates main\n" | sudo tee sources.list;;
-    3 ) echo "Buster (Testing)"; cd /etc/apt/; sudo cp sources.list sources.list.bak; echo "###### Debian Buster (Testing)\ndeb http://deb.debian.org/debian/ testing main contrib non-free\ndeb-src http://deb.debian.org/debian/ testing main contrib non-free\n# Security\ndeb http://deb.debian.org/debian-security testing/updates main\ndeb-src http://deb.debian.org/debian-security testing/updates main\n" | sudo tee sources.list;;
-    4 ) echo "Sid (Unstable)"; cd /etc/apt/; sudo cp sources.list sources.list.bak;  echo "###### Debian Main Repos\ndeb http://deb.debian.org/debian/ oldstable main contrib non-free\ndeb http://deb.debian.org/debian-security oldstable/updates main\n" | sudo tee sources.list;;
-    5 ) echo "Roll backup"; cd /etc/apt/; sudo cp sources.list.bak sources.list -fv;;
+    1 ) echo "Jessie (Oldstable)";
+        cd /etc/apt/;
+        sudo cp sources.list sources.list.bak;
+        echo "# Debian Jessie (Oldstable)\ndeb http://deb.debian.org/debian/ oldstable main contrib non-free\n#Security\ndeb http://deb.debian.org/debian-security oldstable/updates main\n" | sudo tee sources.list;; 
+
+    2 ) echo "Stretch (Stable)";
+        cd /etc/apt/;
+        sudo cp sources.list sources.list.bak;
+        echo "# Debian Stretch (Stable)\ndeb http://deb.debian.org/debian/ stable main contrib non-free\ndeb-src http://deb.debian.org/debian/ stable main contrib non-free\n# Security\ndeb http://deb.debian.org/debian-security stable/updates main\ndeb-src http://deb.debian.org/debian-security stable/updates main\n" | sudo tee sources.list;;
+
+    3 ) echo "Buster (Testing)";
+        cd /etc/apt/;
+        sudo cp sources.list sources.list.bak;
+        echo "###### Debian Buster (Testing)\ndeb http://deb.debian.org/debian/ testing main contrib non-free\ndeb-src http://deb.debian.org/debian/ testing main contrib non-free\n# Security\ndeb http://deb.debian.org/debian-security testing/updates main\ndeb-src http://deb.debian.org/debian-security testing/updates main\n" | sudo tee sources.list;;
+
+    4 ) echo "Sid (Unstable)";
+        cd /etc/apt/;
+        sudo cp sources.list sources.list.bak;
+        echo "###### Debian Main Repos\ndeb http://deb.debian.org/debian/ oldstable main contrib non-free\ndeb http://deb.debian.org/debian-security oldstable/updates main\n" | sudo tee sources.list;;
+
+    5 ) echo "Roll backup";
+        cd /etc/apt/;
+        sudo cp sources.list.bak sources.list -fv;;
+
     n|N ) echo "No";;
 esac
 echo
@@ -59,17 +78,15 @@ echo
 case "$option" in
     y|Y ) echo "Yes";
         sudo apt update;
-        echo "Installing Xorg";
         sudo apt install xorg -y;
-        echo "Installing Awesome";
         sudo apt install awesome -y;
-        echo "Copying configs"
         sudo mkdir -pv /home/$1/.config/awesome/;
         sudo cp /etc/xdg/awesome/rc.lua /home/$1/.config/awesome/ -fv;
         sudo chown $1 /home/$1/.config/awesome/rc.lua -v
         cd /home/$1/.config/awesome 
         cp rc.lua rc.lua.bak -v
-        cp /home/$1/.dotfiles/files/rc.lua . -v;;        
+        cp /home/$1/.dotfiles/files/rc.lua . -v;;
+        
     n|n ) echo "No";;
 esac
 echo
@@ -78,10 +95,18 @@ echo
 read -p "$(echo -e 'What Packages ?\n1: Laptop\n2: Workstation\n3: Server\n4: Minimal Server\n\b')" option
 echo
 case "$option" in
-    1 ) echo "Laptop"; sudo apt install fail2ban rofi neofetch vim mpv screen pulseaudio pavucontrol tmux python3-dev python3-pip python-dev python-pip alsa-utils rxvt-unicode-256color zsh moc virtualenv virtualenvwrapper dirmngr xbacklight wicd-curses firmware-iwlwifi -y; ln -svf /home/$1/.dotfiles/configs/70-synaptics.conf /etc/X11/xorg.conf.d/70-synaptics.conf;;
-    2 ) echo "Workstation"; sudo apt install fail2ban rofi neofetch vim mpv screen pulseaudio pavucontrol tmux python3-dev python3-pip python-dev python-pip alsa-utils rxvt-unicode-256color zsh moc virtualenv virtualenvwrapper dirmngr -y;;
-    3 ) echo "Server"; sudo apt install fail2ban neofetch vim screen tmux python3-dev python3-pip python-dev python-pip zsh virtualenv virtualenvwrapper dirmngr -y;;
-    4 ) echo "Minimal Server"; sudo apt install fail2ban neofetch screen tmux zsh vim -y;;
+    1 ) echo "Laptop"; 
+        sudo apt install fail2ban rofi neofetch vim mpv screen pulseaudio pavucontrol tmux python3-dev python3-pip python-dev python-pip alsa-utils rxvt-unicode-256color zsh moc virtualenv virtualenvwrapper dirmngr xbacklight wicd-curses firmware-iwlwifi -y;
+        ln -svf /home/$1/.dotfiles/configs/70-synaptics.conf /etc/X11/xorg.conf.d/70-synaptics.conf;;
+    
+    2 ) echo "Workstation"; 
+        sudo apt install fail2ban rofi neofetch vim mpv screen pulseaudio pavucontrol tmux python3-dev python3-pip python-dev python-pip alsa-utils rxvt-unicode-256color zsh moc virtualenv virtualenvwrapper dirmngr -y;;
+    
+    3 ) echo "Server"; 
+        sudo apt install fail2ban neofetch vim screen tmux python3-dev python3-pip python-dev python-pip zsh virtualenv virtualenvwrapper dirmngr -y;;
+    
+    4 ) echo "Minimal Server";
+        sudo apt install fail2ban neofetch screen tmux zsh vim -y;;
     n|N ) echo "No";;
 esac
 echo
@@ -90,8 +115,12 @@ echo
 read -p "$(echo -e 'Setup Unattended Upgrades ?\n1: 20auto-upgrades,\n2: 02periodic\n\b')" option
 echo
 case "$option" in
-    1 ) echo "20auto-upgrades";sudo apt install unattended-upgrades apt-listchanges -y; echo 'APT::Periodic::Update-Package-Lists "1";\nAPT::Periodic::Unattended-Upgrade "1";' | sudo tee -a /etc/apt/apt.conf.d/20auto-upgrades;;
-    2 ) echo "02periodic";echo "#TODO";;
+    1 ) echo "20auto-upgrades";
+        sudo apt install unattended-upgrades apt-listchanges -y; 
+        echo 'APT::Periodic::Update-Package-Lists "1";\nAPT::Periodic::Unattended-Upgrade "1";' | sudo tee -a /etc/apt/apt.conf.d/20auto-upgrades;;
+    
+    2 ) echo "02periodic";
+        echo "#TODO";;
     n|N ) echo "No";;
 esac
 echo
@@ -100,9 +129,15 @@ echo
 read -p "$(echo -e 'What Driver ?\n1: NVIDIA (Without Optimus)\n2: NVIDIA (With Optimus)\n3: AMD\n\b')" option
 echo
 case "$option" in
-    1 ) echo "NVIDIA (Without Optimus)"; echo "#TODO";; #echo "# stretch-backports\ndeb http://httpredir.debian.org/debian stretch-backports main contrib non-free" | sudo tee -a && apt-get install linux-headers-$(uname -r|sed 's/[^-]*-[^-]*-//') && sudo apt update && apt install -t stretch-backports nvidia-driver -y
-    2 ) echo "NVIDIA (With Optimus)"; echo "#TODO";;
-    3 ) echo "AMD"; sudo apt update && sudo apt install firmware-amd-graphics -y;;
+    1 ) echo "NVIDIA (Without Optimus)"; 
+        echo "#TODO";; #echo "# stretch-backports\ndeb http://httpredir.debian.org/debian stretch-backports main contrib non-free" | sudo tee -a && apt-get install linux-headers-$(uname -r|sed 's/[^-]*-[^-]*-//') && sudo apt update && apt install -t stretch-backports nvidia-driver -y
+    
+    2 ) echo "NVIDIA (With Optimus)";
+        echo "#TODO";;
+    
+    3 ) echo "AMD"; 
+        sudo apt update;
+        sudo apt install firmware-amd-graphics -y;;
     n|N ) echo "No";;
 esac
 echo
@@ -118,6 +153,7 @@ case "$option" in
         git clone https://github.com/robbyrussell/oh-my-zsh.git /home/$1/.oh-my-zsh;
         vim +PluginInstall +qall;
         ln -sfv $dir/Trilambda.zsh-theme /home/$1/.oh-my-zsh/themes/Trilambda.zsh-theme;;
+
     n|n ) echo "No";;
 esac
 echo
@@ -126,7 +162,12 @@ echo
 read -p "Install Golang? y/n " option
 echo
 case "$option" in
-    y|Y ) echo "Yes";cd /tmp && wget -q -O golang_tar https://dl.google.com/go/go1.10.2.linux-amd64.tar.gz && tar xvf golang_tar && sudo mv go /usr/local;;
+    y|Y ) echo "Yes";
+        cd /tmp;
+        wget -q -O golang_tar https://dl.google.com/go/go1.10.2.linux-amd64.tar.gz;
+        tar xvf golang_tar;
+        sudo mv go /usr/local;;
+
     n|n ) echo "No";;
 esac
 echo
@@ -136,7 +177,28 @@ echo
 read -p "Install Telegram?  y/n " option
 echo
 case "$option" in
-    y|Y ) echo "Install"; cd /tmp && wget -q -O tg_tar https://telegram.org/dl/desktop/linux && tar xvf tg_tar && sudo mv Telegram /opt/Telegram && sudo chown $1:$1 /opt/Telegram -R && sudo ln -fvs /opt/Telegram/Telegram /bin/Telegram && sudo ln -fs /opt/Telegram/Updater /bin/Updater;;
+    y|Y ) echo "Install"; 
+        cd /tmp 
+        wget -q -O tg_tar https://telegram.org/dl/desktop/linux;
+        tar xvf tg_tar;
+        sudo mv Telegram /opt/Telegram;
+        sudo chown $1:$1 /opt/Telegram -R;
+        sudo ln -fvs /opt/Telegram/Telegram /bin/Telegram;
+        sudo ln -fs /opt/Telegram/Updater /bin/Updater;;
+
+    n|N ) echo "No";;
+esac
+echo
+
+##Install Slack
+read -p "Install Slack?  y/n " option
+echo
+case "$option" in
+    y|Y ) echo "Install"; 
+        cd /tmp;
+        wget -q -O slack.deb https://downloads.slack-edge.com/linux_releases/slack-desktop-3.2.0-beta25a7a50e-amd64.deb;
+        sudo dpkg -i slack.deb;;
+
     n|N ) echo "No";;
 esac
 echo
@@ -145,7 +207,12 @@ echo
 read -p "Install Firefox?  y/n " option
 echo
 case "$option" in
-    y|Y ) echo "Install"; cd /tmp && wget -q -O firefax_tar https://download.mozilla.org/\?product\=firefox-latest-ssl\&os\=linux64\&lang\=en-US && tar xvf firefax_tar && sudo mv firefox /opt/ && sudo chown $1:$1 /opt/firefox -R && sudo ln -fvs /opt/firefox/firefox /bin/firefox;;
+    y|Y ) echo "Install"; cd /tmp 
+        wget -q -O firefax_tar https://download.mozilla.org/\?product\=firefox-latest-ssl\&os\=linux64\&lang\=en-US;
+        tar xvf firefax_tar;
+        sudo mv firefox /opt/;
+        sudo chown $1:$1 /opt/firefox -R;
+        sudo ln -fvs /opt/firefox/firefox /bin/firefox;;
     n|N ) echo "No";;
 esac
 echo
@@ -154,7 +221,15 @@ echo
 read -p "Install Docker?  y/n " option
 echo
 case "$option" in
-    y|Y ) echo "Install"; cd /tmp && sudo apt update && sudo apt install apt-transport-https ca-certificates curl gnupg2 software-properties-common -y && curl -fsSL https://download.docker.com/linux/$(. /etc/os-release; echo "$ID")/gpg | sudo apt-key add - && curl -fsSL https://download.docker.com/linux/$(. /etc/os-release; echo "$ID")/gpg | sudo apt-key add - && sudo $(curl -s https://docs.docker.com/install/linux/docker-ce/debian/\#set-up-the-repository | egrep "(apt-key fingerprint ([0-9A-F]{8}))" | cut -d'>' -f 9) && sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/$(. /etc/os-release; echo "$ID") $(lsb_release -cs) stable" && sudo apt update && sudo apt install docker-ce -y;;
+    y|Y ) echo "Install"; 
+        cd /tmp 
+        sudo apt update;
+        sudo apt install apt-transport-https ca-certificates curl gnupg2 software-properties-common -y;
+        curl -fsSL https://download.docker.com/linux/$(. /etc/os-release; echo "$ID")/gpg | sudo apt-key add -;
+        sudo $(curl -s https://docs.docker.com/install/linux/docker-ce/debian/\#set-up-the-repository | egrep "(apt-key fingerprint ([0-9A-F]{8}))" | cut -d'>' -f 9);
+        sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/$(. /etc/os-release; echo "$ID") $(lsb_release -cs) stable";
+        sudo apt update;
+        sudo apt install docker-ce -y;;
     n|N ) echo "No";;
 esac
 echo
@@ -163,7 +238,10 @@ echo
 read -p "Install Docker-compose?  y/n " option
 echo
 case "$option" in
-    y|Y ) echo "Install"; cd /tmp && sudo curl -L https://github.com/docker/compose/releases/download/1.21.2/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose && sudo chmod +x /usr/local/bin/docker-compose;;
+    y|Y ) echo "Install";
+        cd /tmp;
+        sudo curl -L https://github.com/docker/compose/releases/download/1.21.2/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose;
+        sudo chmod +x /usr/local/bin/docker-compose;;
     n|N ) echo "No";;
 esac
 echo
@@ -173,8 +251,16 @@ echo
 read -p "$(echo -e 'Install/ Update Spotify?\n1: Install\n2: Update\n\b')" option
 echo
 case "$option" in
-    1 ) echo "Install"; $(wget -qO- https://www.spotify.com/no/download/linux | egrep 'recv-keys\s\w+') && echo deb http://repository.spotify.com stable non-free | sudo tee /etc/apt/sources.list.d/spotify.list && sudo apt update && sudo apt install spotify-client -y ;;
-    2 ) echo "Update"; $(wget -qO- https://www.spotify.com/no/download/linux | egrep 'recv-keys\s\w+') && sudo apt update && sudo apt install spotify-client -y ;;
+    1 ) echo "Install"; 
+        $(wget -qO- https://www.spotify.com/no/download/linux | egrep 'recv-keys\s\w+');
+        echo deb http://repository.spotify.com stable non-free | sudo tee /etc/apt/sources.list.d/spotify.list;
+        sudo apt update;
+        sudo apt install spotify-client -y;;
+
+    2 ) echo "Update"; 
+        $(wget -qO- https://www.spotify.com/no/download/linux | egrep 'recv-keys\s\w+');
+        sudo apt update;
+        sudo apt install spotify-client -y;;
     n|N ) echo "No";;
 esac
 echo
@@ -183,7 +269,10 @@ echo
 read -p "Install Displaylink?  y/n " option
 echo
 case "$option" in
-    y|Y ) echo "Install"; cd /tmp && wget -q https://raw.githubusercontent.com/AdnanHodzic/displaylink-debian/master/displaylink-debian.sh && sudo bash displaylink-debian.sh;;
+    y|Y ) echo "Install"; 
+        cd /tmp
+        wget -q https://raw.githubusercontent.com/AdnanHodzic/displaylink-debian/master/displaylink-debian.sh;
+        sudo bash displaylink-debian.sh;;
     n|N ) echo "No";;
 esac
 echo
@@ -193,16 +282,23 @@ echo
 read -p "Install skandix toolbox of somewhat working scripts ?  y/n " option
 echo
 case "$option" in
-    y|Y ) echo "Install"; cd $HOME/.dotfiles && sudo cp scripts /opt/scripts && sudo chown $1:$1 /opt/scripts -R && echo "symlink and crontab you self, have to fix..." ;;
+    y|Y ) echo "Install";
+        cd $HOME/.dotfiles;
+        sudo cp scripts /opt/scripts;
+        sudo chown $1:$1 /opt/scripts -R;
+        echo "symlink and crontab you self, have to fix..." ;;
     n|N ) echo "No";;
 esac
 echo
+
 
 ##remove beeping module
 read -p "Remove the fucking beeping ?  y/n " option
 echo
 case "$option" in
-    y|Y ) echo "Install"; sudo modprobe -r pcspkr;echo "# Do not load 'pcspkr' module on boot\n#blacklist pcspkr" | sudo tee -a /etc/modprobe.d/nobeep.conf;;
+    y|Y ) echo "Install";
+        sudo modprobe -r pcspkr;
+        echo "# Do not load 'pcspkr' module on boot\n#blacklist pcspkr" | sudo tee -a /etc/modprobe.d/nobeep.conf;;
     n|N ) echo "No";;
 esac
 echo
@@ -211,7 +307,14 @@ echo
 read -p "Do you want to allow, Permission fix and symlink of rc.lua and neofetch confg ?  y/n " option
 echo
 case "$option" in
-    y|Y ) echo "";sudo mv /etc/motd /etc/motd.back;sudo chown $1:$1 /home/$1 -R;cd /home/$1/.config/awesome/;cp rc.lua rc.lua.bak;ln -svf /home/$1/.dotfiles/files/rc.lua /home/$1/.config/awesome/rc.lua -rfv;ln -svf /home/$1/.dotfiles/files/config_neofetch /home/$1/.config/neofetch/config;;
+    y|Y ) echo "Install";
+        sudo mv /etc/motd /etc/motd.back;
+        sudo chown $1:$1 /home/$1 -R;
+        cd /home/$1/.config/awesome/;
+        cp rc.lua rc.lua.bak;
+        ln -svf /home/$1/.dotfiles/files/rc.lua /home/$1/.config/awesome/rc.lua -rfv;
+        ln -svf /home/$1/.dotfiles/files/config_neofetch /home/$1/.config/neofetch/config;;
+    
     n|N ) echo "No";;
 esac
 echo
