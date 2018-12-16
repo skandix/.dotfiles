69a53a98901878fe7c805b538e559ca2a56db84b
Bendik λ <skandix@datapor.no>
2018-12-16 14:24:36 +0100

    there fixed

--- INSTALL.sh
+++ INSTALL.sh
@@ -180,6 +180,7 @@ case "$option" in
 			echo "$red[Error]: Can't find .oh-my-zsh, cloning repo$normie\n"
 			git clone https://github.com/robbyrussell/oh-my-zsh.git /home/$USER/.oh-my-zsh;
 			ln -sf $misc/Trilambda.zsh-theme /home/$USER/.oh-my-zsh/themes/Trilambda.zsh-theme;
+			chsh -s /bin/zsh #Making zsh default shell.
 		fi;;
 	n|n|* ) echo "$red No";;
 esac
