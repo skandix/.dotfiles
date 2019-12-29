44bf6936073a603ca1a1b9c137ac0765d353d7cd
Bendik Egenes Dyrli <bendid13@student.uia.no>
2019-12-29 16:25:17 +0100

    cleaned up wallpapers, and make a few scripts to make it easier to update/set wallpapers when i get on  new machine

---
+++ scripts/update_wall.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+#theme.wallpaper\ \=\ [\S]+
+#sed -i 's+'
+#${parameter//pattern/string}
+#old='theme.wallpaper = themes_path.."default/background.png"'
+#new='theme.wallpaper = os.getenv("HOME").."/.dotfiles/files/misc/wall.jpg'
+#cat /usr/share/awesome/themes/default/theme.lua | sed 's:/$old/$new/g'
