d701c9fa6f176dc84f0be824dbecad9ed44e7341
Bendik Egenes Dyrli <bendik.dyrli@uia.no>
2020-07-08 18:18:08 +0200

    added jackdconfig + script to clone all repos in a github org where all repos are public
    Signed-off-by: Bendik Egenes Dyrli <bendik.dyrli@uia.no>

---
+++ scripts/git_clone_org.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+org=$1
+mkdir $org"_org" && cd $org"_org"
+for url in $(curl https://api.github.com/orgs/$org/repos?per_page=200 | grep 'ssh_url' | cut -d':' -f 2-3 | tr -d \",); do
+	git clone $url;
+done;
+cd ..;
