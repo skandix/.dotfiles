b082e14f479065909593ba5f5f09078058aa2bde
Bendik Egenes Dyrli <bendik.dyrli@uia.no>
2020-08-27 09:00:14 +0200

    added stpaming in commit
    Signed-off-by: Bendik Egenes Dyrli <bendik.dyrli@uia.no>

--- scripts/_Obsidian.sh
+++ scripts/_Obsidian.sh
@@ -1,2 +1,4 @@
 #!/usr/bin/env bash
-cd && cd Documents/ && git pull && obsidian && git add . && git commit -s -m " Auto Commit on Close  " && git push
+host=$HOSTNAME
+date=$(date | cut -d' ' -f 1-4)
+cd && cd Documents/ && git pull && obsidian && git add . && git commit -s -m " Auto Commit on Close at $host - $date  " && git push
