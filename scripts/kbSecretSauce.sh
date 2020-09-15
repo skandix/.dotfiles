d4ab5d6eec36fbe07a9d1dd88b8bfedcf85a3956
Bendik Egenes Dyrli <bendik.dyrli@uia.no>
2020-09-15 16:57:01 +0200

    sfisdffa
    Signed-off-by: Bendik Egenes Dyrli <bendik.dyrli@uia.no>

--- scripts/kbSecretSauce.sh
+++ scripts/kbSecretSauce.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/env bash
-dir=/home/skandix/Pictures/screenshot;
-localFile="$(deepin-screenshot -s $dir 2>&1 --no-notification | grep "savepath2" | cut -d\" -f2)";
+name=$(cat /proc/sys/kernel/random/uuid).jpg
+
+dir=/home/skandix/Pictures/screenshot
+localFile="$(deepin-screenshot -s $dir 2>&1 --no-notification | grep "savepath2" | cut -d\" -f2)"
+
 echo $localFile
-xclip -selection clipboard -t image/png -i $localFile
+scp -q $name loot:/var/www/html/loot
+echo http://datapor.no/loot/$name | xclip
+
+
+#xclip -selection clipboard -t image/png -i $localFile
