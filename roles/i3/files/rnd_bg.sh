b76967ac5bf6532d03ccc10182ecedbc4daea30f
Bendik Dyrli <skandix@datapor.no>
2022-10-12 13:17:42 +0200

    fix bg issue
    Signed-off-by: Bendik Dyrli <skandix@datapor.no>

--- roles/i3/files/rnd_bg.sh
+++ roles/i3/files/rnd_bg.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 path="/home/$USER/.dotfiles/roles/i3/files/bg"
-feh "$(find "$path" | shuf -n 1 )" --bg-scale && wal -i "$(find "$path" | shuf -n 1 )"
+one_in_the_bag=$(find "$path" | shuf -n 1 )
+feh $one_in_the_bag --bg-scale && wal -i $one_in_the_bag
