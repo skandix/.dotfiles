02a2e144dd2ec1deb2d81c7102f355fa785d3902
Bendik Dyrli <skandix@protonmail.com>
2023-08-21 01:10:48 +0200

    cleanup some wallpapers, and added a seperate role for discord so it can symlink it's config file so we avoid that it breaks before a update is present in the repo
    Signed-off-by: Bendik Dyrli <skandix@protonmail.com>

---
+++ roles/discord/files/wezterm.lua
@@ -0,0 +1,35 @@
+local wezterm = require 'wezterm'
+
+local config = {}
+
+if wezterm.config_builder then
+  config = wezterm.config_builder()
+end
+
+-- This is where you actually apply your config choices
+config.automatically_reload_config = true
+config.front_end = "OpenGL"
+config.disable_default_key_bindings = true
+config.use_dead_keys = false
+config.warn_about_missing_glyphs = false
+config.enable_wayland = false
+config.hide_tab_bar_if_only_one_tab = true -- since i use i3 i do not need tabs as much as i would i.e windows
+config.bold_brightens_ansi_colors = true
+config.window_close_confirmation = "NeverPrompt"
+
+
+config.window_frame = {
+	font_size  = 8.0,
+}
+
+
+
+config.inactive_pane_hsb = {
+  saturation = 0.9,
+  brightness = 0.8,
+}
+
+
+-- and finally, return the configuration to wezterm
+return config
+
