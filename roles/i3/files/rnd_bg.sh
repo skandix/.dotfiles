945c29ec816e0e876cb8c70067899b51c08e5137
Bendik Dyrli <skandix@protonmail.com>
2023-08-21 02:11:55 +0200

    #2 fixed
    Signed-off-by: Bendik Dyrli <skandix@protonmail.com>

--- roles/i3/files/rnd_bg.sh
+++ roles/i3/files/rnd_bg.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-path="/home/$USER/.dotfiles/roles/i3/files/bg"
+path="$HOME/.wallpapers/"
 one_in_the_bag=$(find "$path" | shuf -n 1 )
 feh $one_in_the_bag --bg-scale && wal -i $one_in_the_bag
